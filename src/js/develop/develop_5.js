e1aa673e249e01fa68933e1a5f544c32f37b8c51
nickolay Gotsliyk <trolizmaslom@gmail.com>
2016-05-06 09:58:52 +0300

    wefdsf

--- src/js/develop/develop_5.js
+++ src/js/develop/develop_5.js
@@ -1,8 +1,15 @@
 function heightForJscroll(obj) {
-    var contentHeight = $('.second-block .content').outerHeight();
-    var buttonHeight = $('.second-block .button-more').outerHeight() + parseInt( $('.second-block .button-more').css('bottom'))+40;
-    var titleHeight = obj.find('.ttl').outerHeight();
-    var needHeiht= contentHeight - buttonHeight - titleHeight;
+    if($(window).width>767) {
+        var contentHeight = $('.second-block .content').outerHeight();
+        var buttonHeight = $('.second-block .button-more').outerHeight() + parseInt($('.second-block .button-more').css('bottom')) + 40;
+        var titleHeight = obj.find('.ttl').outerHeight();
+        var needHeiht = contentHeight - buttonHeight - titleHeight;
+    }else{
+        var contentHeight = $('.second-block .right').outerHeight();
+        var buttonHeight = $('.second-block .button-more').outerHeight() + parseInt($('.second-block .button-more').css('bottom')) + 40;
+        var titleHeight = obj.find('.ttl').outerHeight();
+        var needHeiht = contentHeight - buttonHeight - titleHeight;
+    }
     obj.find('.txt').height(needHeiht);
 
 }
@@ -30,11 +37,38 @@ function startHeight() {
     });
 
 }
+function slider() {
+    $('.second-block .buttons-nav .butt').click(function (e) {
+        e.preventDefault();
+        var tabs = $('.second-block .tabs');
+        var infos = $('.second-block .info');
+        var active = $('.second-block .tabs.active').index();
+        var ind = active;
+        if($(this).hasClass('prev')){
+            tabs.removeClass('active');
+            infos.removeClass('active');
+            var ind = active - 1;
+            if(active == 0){ind = tabs.length - 1;}
+            infos.eq(ind).addClass('active');
+            tabs.eq(ind).addClass('active');
+        }
+        if($(this).hasClass('next')){
+            tabs.removeClass('active');
+            infos.removeClass('active');
+            var ind = active + 1;
+            if(active == tabs.length - 1){ind = 0;}
+            infos.eq(ind).addClass('active');
+            tabs.eq(ind).addClass('active');
+        }
+    });
+
+}
 
 $(document).ready(function(){
     startHeight();
     tabsChoise();
     $('.second-block .info .txt').jScrollPane();
+    slider();
 });
 
 $(window).load(function(){
